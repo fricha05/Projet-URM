1bc4c1b4e946e9279d9a0d8f8fda0d8ea7500eec
fricha05 <fricha05@etud.u-pem.fr>
2018-05-04 09:45:41 +0200

    compile_comment_out

---
+++ projet-florian.ml
@@ -0,0 +1,14 @@
+let rec compile_comment_out is =
+  match is with
+  |[] -> []
+  |Comment s::is' -> compile_comment_out is'
+  |i::is' -> i::compile_comment_out is'
+;;
+
+let compile_comment_out' is =
+  let is_not_comment i =
+    match i with
+    |Comment _ -> false
+    |_ -> true
+    in List.filter is_not_comment is
+;;
