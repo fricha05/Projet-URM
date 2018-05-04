c7343c795b12fe1df0359caef4043afb684c1018
fricha05 <fricha05@etud.u-pem.fr>
2018-05-04 10:14:21 +0200

    corrections ctrlf

--- projet-florian.ml
+++ projet-florian.ml
@@ -1,14 +1,19 @@
 let rec compile_comment_out is =
-  match is with
-  |[] -> []
-  |Comment s::is' -> compile_comment_out is'
-  |i::is' -> i::compile_comment_out is'
+    match is with
+    |[] -> []
+    |Comment s::is' -> compile_comment_out is'
+    |i::is' -> i::compile_comment_out is'
 ;;
 
 let compile_comment_out' is =
-  let is_not_comment i =
-    match i with
-    |Comment _ -> false
-    |_ -> true
-    in List.filter is_not_comment is
+    let is_not_comment i =
+        match i with
+        |Comment _ -> false
+        |_ -> true
+        in List.filter is_not_comment is
 ;;
+(* 
+let rec compile_regs_out is =
+    match List.hd is with
+    |[] -> []
+    | *)
