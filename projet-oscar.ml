051e659709029c5c92c559b919b0ce3ca1459918
Oscar Falmer <oscar.falmer@gmail.com>
2018-05-04 12:19:01 +0200

    Etape 2 presque finie

--- projet-oscar.ml
+++ projet-oscar.ml
@@ -22,7 +22,8 @@ let rec dec_out is =
 let rec mult_out is =
     match is with
     |[] -> []
-    |Mult(ri1,ri2)::is' -> Add(ri1,ri1)::mult_out (Mult(ri1, (ri2-1))::is')
+    |Mult(ri1,ri2)::is' -> if r1 <> 1 then Add(ri1,ri1)::mult_out (Mult(ri1, (ri2-1))::is')
+    						else mult_out is'
     |i::is' -> i::mult_out is'
 ;;
 
@@ -54,6 +55,37 @@ let rec ltpredicate_out is =
 	|i::is' -> i::ltpredicate_out is'
 ;;
 
+let compile_stage1 is = ltpredicate_out (leqpredicate_out (geqpredicate_out (zero_predicate_out (mult_out (dec_out is)))));;
+
+// Etape 2
+
+let rec add_out is = 
+	match is with
+	|[] -> []
+	|Add(ri1,ri2)::is' -> if r2 <> 0 then Inc(ri1)::add_out (Add(ri1, (ri2-1))::is')
+    						else add_out is'
+	|i::is' -> i::ltpredicate_out is'
+;;
+
+let rec sub_out is = 
+	match is with
+	|[] -> []
+	|Sub(ri1,ri2)::is' -> Add(0,(ri1-ri2)::sub_out is'
+	|i::is' -> i::sub_out is'
+;;
+
+let rec gtpredicate_out is = 
+	match is with
+	|[] -> []
+	|GTqPredicate(ri1, ri2, label)::is' -> Add(0,(ri1-ri2)::gtpredicate_out is'
+	|i::is' -> i::gtpredicate_out is'
+;;
+
+let compile_stage2 is = gtpredicate_out( sub_out( add_out(is) ) );;
+
+// Etape 3
+
+// Etape 4
 
 
 
